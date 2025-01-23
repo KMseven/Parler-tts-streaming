fae11cd4d8eeb891b23f0ceddf5d0c46e5fb3955
Krishna Mohan <10342085+KMseven@users.noreply.github.com>
2025-01-24 02:29:26 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,13 +3,13 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["Hello, how are you doing today?"]
+        'example': ["A male speaker with a low-pitched voice delivering his words at a fast pace in a small, confined space with a very clear audio and an animated tone."]
     },
     "input_value": {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["Leela speaks in a high-pitched, fast-paced, and cheerful tone, full of energy and happiness. The recording is very high quality with no background noise."]
+        'example': ["Remember - this is only the first iteration of the model! To improve the prosody and naturalness of the speech further, we're scaling up the amount of training data by a factor of five times."]
     }
 }
 IS_STREAMING_OUTPUT = True
